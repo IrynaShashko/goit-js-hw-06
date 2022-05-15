efe15ac1b9c109c4c3d4219caf8fcecf960589a7
IrynaShashko <irishkashashko@gmail.com>
2022-05-15 21:24:43 +0300

    add 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const colorBtn = document.querySelector(".change-color");
+const backgroundColor = document.querySelector(".color");
+
+const changeColorBtn = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  backgroundColor.textContent = body.style.backgroundColor;
+}
+
+colorBtn.addEventListener("click", changeColorBtn);
+
