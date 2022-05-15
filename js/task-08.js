cfe44471a0eefc0204195baabeb3f247f5a777bd
IrynaShashko <irishkashashko@gmail.com>
2022-05-15 19:19:08 +0300

    add 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+ form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return console.log("Please fill in all the fields!");
+    } else {
+        const formData = {
+            email: email.value,
+            password: password.value,
+        }
+        console.log(formData);
+    }
+    
+  event.currentTarget.reset();
+}
