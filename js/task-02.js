51ede0e8aa493453f8791c0c09c0d34bf95b5f41
IrynaShashko <irishkashashko@gmail.com>
2022-05-17 10:49:10 +0300

    add 2task

--- js/task-02.js
+++ js/task-02.js
@@ -7,11 +7,11 @@ const ingredients = [
   'Condiments',
 ];
 
-ingredients.forEach(el => {
+const element = ingredients.map(el => {
   const ingredient = document.createElement("li");
   ingredient.classList.add("item");
   ingredient.textContent = el;
+  return ingredient;  
+})  
   const ingredientsList = document.querySelector("#ingredients");
-  ingredientsList.append(ingredient);
-    }
-  )
+  ingredientsList.append(...element);
